861367a322963437ff6835c599a94c83b90502e0
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2022-11-26 19:14:57 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+
+
+import Button from '@mui/material/Button';
+
 
 function App() {
+  const[counter,setCounter]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className='APP'  style={{
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '300%',
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  top: '-15%',
+}}
+ >Counter App
+  <div style={{
+        fontSize: '120%',
+        position: 'relative',
+        top: '10vh',
+      }}>
+        {counter}
+      </div>
+
+
+
+ <div className='Buttons'>
+
+<Button variant="contained"  style={{
+ position: 'relative',
+ top: '20vh',
+ marginRight: '5px',}}
+onClick={()=>
+setCounter(counter+1)
+}>Increment </Button>
+
+<Button variant="contained" 
+style={{
+  position: 'relative',
+  top: '20vh',
+  marginRight: '5px',}}
+onClick={()=>{
+  if(counter!==0){
+    setCounter(counter-1)
+  }
+}
+}>Decrement</Button>
+
+ </div>
+ </div>
   );
 }
 
